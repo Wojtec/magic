2afd365eaf082d9b03b7121ec50385e3f5658510
wojtec <poninski.w@gmail.com>
2025-02-27 16:26:19 +0100

    feat: add routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { FC } from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { CollectionsPage } from './modules/collections';
 
 const App: FC = () => {
-  return <div className='App'>Hello magic</div>;
+  return (
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+      <Routes>
+        <Route path='/collections' element={<CollectionsPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/modules/collections/containers/CollectionsPage.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const CollectionsPage: FC = () => {
+  return <div>Collections Page !</div>;
+};
