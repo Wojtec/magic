48e72521928a463ac0bb45effa717cd003d22d68
wojtec <poninski.w@gmail.com>
2025-03-01 20:22:52 +0100

    feat: deploy gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ghPages()],
   publicDir: 'public',
 });
